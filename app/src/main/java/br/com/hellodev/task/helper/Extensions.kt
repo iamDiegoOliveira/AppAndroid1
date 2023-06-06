c2b5c992725d0fb0e9efa5216c5589c06efcb3b7
iamDiegoOliveira <115094058+iamDiegoOliveira@users.noreply.github.com>
2023-06-05 22:20:32 -0300

    Update Extensions.kt

--- app/src/main/java/br/com/hellodev/task/helper/Extensions.kt
+++ app/src/main/java/br/com/hellodev/task/helper/Extensions.kt
@@ -1,14 +1,14 @@
 package br.com.hellodev.task.helper
 
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import androidx.fragment.app.Fragment
-import br.com.hellodev.task.R
-import br.com.hellodev.task.databinding.BottomSheetBinding
-import com.google.android.material.bottomsheet.BottomSheetDialog
+ import android.graphics.Color
+ import androidx.appcompat.app.AppCompatActivity
+ import androidx.appcompat.widget.Toolbar
+ import androidx.fragment.app.Fragment
+ import br.com.hellodev.task.R
+ import br.com.hellodev.task.databinding.BottomSheetBinding
+ import com.google.android.material.bottomsheet.BottomSheetDialog
 
-fun Fragment.initToolbar(toolbar: Toolbar) {
+ fun Fragment.initToolbar(toolbar: Toolbar) {
     (activity as AppCompatActivity).setSupportActionBar(toolbar)
     (activity as AppCompatActivity).title = ""
     (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
@@ -16,14 +16,14 @@ fun Fragment.initToolbar(toolbar: Toolbar) {
     toolbar.setTitleTextColor(Color.parseColor("#FFFFFF"))
 }
 
-fun Fragment.showBottomSheet(
+ fun Fragment.showBottomSheet(
     titleDialog: Int? = null,
     titleButton: Int? = null,
     message: Int,
     onClick: () -> Unit = {}
 ) {
-    val bottomSheetDialog = BottomSheetDialog(requireContext(), R.style.BottomSheetDialog)
-    val bottomSheetBinding: BottomSheetBinding =
+     val bottomSheetDialog = BottomSheetDialog(requireContext(), R.style.BottomSheetDialog)
+     val bottomSheetBinding: BottomSheetBinding =
         BottomSheetBinding.inflate(layoutInflater, null, false)
 
     bottomSheetBinding.textTitle.text = getString(titleDialog ?: R.string.text_title_bottom_sheet)
