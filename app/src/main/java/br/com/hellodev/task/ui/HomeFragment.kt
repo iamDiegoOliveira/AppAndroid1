2cad0d59072933be48463cdd6b212f597c06d2a1
iamDiegoOliveira <115094058+iamDiegoOliveira@users.noreply.github.com>
2023-06-05 21:50:10 -0300

    Update HomeFragment.kt

--- app/src/main/java/br/com/hellodev/task/ui/HomeFragment.kt
+++ app/src/main/java/br/com/hellodev/task/ui/HomeFragment.kt
@@ -27,7 +27,7 @@ class HomeFragment : Fragment() {
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
-    }
+     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
