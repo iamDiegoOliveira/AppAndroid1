583c796f4b99fa8adb551ec1b3b772d353e3901b
iamDiegoOliveira <115094058+iamDiegoOliveira@users.noreply.github.com>
2023-06-05 21:50:37 -0300

    Update SplashFragment.kt

--- app/src/main/java/br/com/hellodev/task/ui/SplashFragment.kt
+++ app/src/main/java/br/com/hellodev/task/ui/SplashFragment.kt
@@ -27,7 +27,7 @@ class SplashFragment : Fragment() {
     ): View {
         _binding = FragmentSplashBinding.inflate(inflater, container, false)
         return binding.root
-    }
+     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
